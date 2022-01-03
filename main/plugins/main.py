b87ac8b93cca18a96cfa5b7dc10971055819a362
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-03 16:34:17 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -1,16 +1,29 @@
 #tg:ChauhanMahesh/DroneBots
 #github.com/vasusen-code
 
-from .. import Drone, ACCESS_CHANNEL, AUTH_USERS
+import re
+from .. import Drone
 from telethon import events, Button
 from main.plugins.drive import drive
-from telethon.tl.types import MessageMediaWebPage
 
+def get_link(string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)   
+    try:
+        link = [x[0] for x in url]
+        if link:
+            return link
+        else:
+            return False
+    except Exception:
+        return False
+      
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def u(event):
-    if not hasattr(event.media, "webpage"):
+    link = get_link(event.text)
+    if link is False:
         return
-    if 'drive.google.com' in event.media.webpage.url: 
+    if 'drive.google.com' in link: 
         await event.reply('.', buttons=[[Button.inline("Upload.", data="drive")]])
         
 @Drone.on(events.callbackquery.CallbackQuery(data="drive"))
