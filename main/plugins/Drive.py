fdeb076f6a77052cb2467ff1fb9334c280ca256d
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-03 12:44:06 +0530

    Update Drive.py

--- main/plugins/Drive.py
+++ main/plugins/Drive.py
@@ -16,27 +16,74 @@ from LOCAL.localisation import SUPPORT_LINK
 from telethon.errors.rpcerrorlist import MessageNotModifiedError
 from telethon.tl.types import MessageMediaWebPage
 
+def drive_folder_download(url):
+    output = gdown.download_folder(url, quiet=True)
+    return output
+
+async def upload_folder(folder, event, edit):
+    text = f'**UPLOADED by:** {BOT_UN}'
+    index = len(folder)
+    for i in range(int(index)):
+        try:
+            uploader = await fast_upload(f'{folder[i]}', f'{folder[i]}', time.time(), event.client, edit, f'**UPLOADING FILE - {int(i)}:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, force_document=True)
+            os.remove(folder[i])
+        except Exception as e:
+            print(e)
+            return await edit.edit(f"An error [`{e}`] occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    
+    
 async def compress(event, msg):
     cmd = ""
     Drone = event.client
     edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
     link = msg.media.webpage.url
-    output = dt.now().isoformat("_", "seconds") + '.tar.gz'
-    if 'https://drive.google.com/uc?id=' in link:
-        cmd = f'gdown {link} -o {output}'
-    elif 'https://drive.google.com/drive/folders/' in link:
-        cmd = f'gdown {link} -o ./Drive/{event.senderid} --folder'
+    if 'folder' in link:
+        try:
+            folder = drive_folder_download(link)
+        except Exception as e:
+            print(e)
+            return await edit.edit(f"An error [`{e}`] occured while Downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        await upload_folder(folder, event, edit) 
+    elif 'folders' in link:
+        try:
+            folder = drive_folder_download(link)
+        except Exception as e:
+            print(e)
+            return await edit.edit(f"An error [`{e}`] occured while Downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        await upload_folder(folder, event, edit) 
     elif 'https://drive.google.com/file/' in link:
-        x = link.split("/")[5]
-        _link = f'https://drive.google.com/uc?id={x}'
-        cmd = f'gdown {_link} -o {output}'
-    try:
-        await edit.edit("Downloading.")
-        bash(cmd)
-    except Exception as e:
-        print(e)
-        return await edit.edit(f"An error [`{e}`] occured while Downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)  
-    
+        id = (link.split("/"))[5]
+        _link = f'https://drive.google.com/uc?id={id}'
+        try:
+            file = gdown.download(_link, quiet=True)
+            try:
+                uploader = await fast_upload(f'{file}', f'{file}', time.time(), event.client, edit, f'**UPLOADING FILE:**')
+                await Drone.send_file(event.chat_id, uploader, caption=text, force_document=True)
+            except Exception as e:
+                print(e)
+                return await edit.edit(f"An error [`{e}`] occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        except Exception as e:
+            print(e)
+            return await edit.edit(f"An error [`{e}`] occured while Downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        os.remove('file')
+    elif 'https://drive.google.com/uc?id=' in link:
+        try:
+            file = gdown.download(link, quiet=True)
+            try:
+                uploader = await fast_upload(f'{file}', f'{file}', time.time(), event.client, edit, f'**UPLOADING FILE:**')
+                await Drone.send_file(event.chat_id, uploader, caption=text, force_document=True)
+            except Exception as e:
+                print(e)
+                return await edit.edit(f"An error [`{e}`] occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        except Exception as e:
+            print(e)
+            return await edit.edit(f"An error [`{e}`] occured while Downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        os.remove('file')
         
-
-
+        
+        
+        
+        
+        
+    
