1e6169cd17aa3c89ae3f57f7d11c871f564395af
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-03 23:38:43 +0530

    Create requests.py

---
+++ main/plugins/requests.py
@@ -0,0 +1,17 @@
+import requests
+import re
+
+#Get filename from content-disposition
+   
+def get_filename_from_cd(cd):
+    if not cd:
+        return None
+    fname = re.findall('filename=(.+)', cd)
+    if len(fname) == 0:
+        return None
+    return fname[0]
+  
+def weburl(url):
+    r = requests.get(url, allow_redirects=True)
+    filename = get_filename_from_cd(r.headers.get('content-disposition'))
+    open(filename, 'wb').write(r.content)
