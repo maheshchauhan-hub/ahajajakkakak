2cf4609c092aed66dc796c27e070850dbf9dd1f5
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-03 14:32:03 +0530

    Update drive.py

--- main/plugins/drive.py
+++ main/plugins/drive.py
@@ -20,22 +20,7 @@ def drive_folder_download(url):
     output = gdown.download_folder(url, quiet=True)
     return output
 
-#uploads a folder 
-#Note:Here is folder is a list of all contents in a folder
-async def upload_folder(folder, event, edit):
-    text = f'**UPLOADED by:** {BOT_UN}'
-    index = len(folder)
-    for i in range(int(index)):
-        try:
-            uploader = await fast_upload(f'{folder[i]}', f'{folder[i]}', time.time(), event.client, edit, f'**UPLOADING FILE - {int(i)}:**')
-            await Drone.send_file(event.chat_id, uploader, caption=text, force_document=True)
-            os.remove(folder[i])
-        except Exception as e:
-            print(e)
-            return await edit.edit(f"An error [`{e}`] occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
-    
-    
-async def compress(event, msg):
+async def drive(event, msg):
     cmd = ""
     Drone = event.client
     edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
