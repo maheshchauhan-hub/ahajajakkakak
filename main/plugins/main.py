d2a86c272c962dad358d433fd565c23aca808348
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-03 21:49:12 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -1,30 +1,19 @@
 #tg:ChauhanMahesh/DroneBots
 #github.com/vasusen-code
 
-import re
 from .. import Drone
 from telethon import events, Button
 from main.plugins.drive import drive
+from main.plugins.utils.utils import get_link
+from LOCLA.localisation import link_animated
 
-def get_link(string):
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url = re.findall(regex,string)   
-    try:
-        link = [x[0] for x in url][0]
-        if link:
-            return link
-        else:
-            return False
-    except Exception:
-        return False
-      
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def u(event):
     link = get_link(event.text)
     if link is False:
         return
     if 'drive.google.com' in link: 
-        await event.reply('.', buttons=[[Button.inline("Upload.", data="drive")]])
+        await event.send_message(event.chat_id, file=link_animated, reply_to=event.id, buttons=[[Button.inline("Upload.", data="drive")]])
         
 @Drone.on(events.callbackquery.CallbackQuery(data="drive"))
 async def d(event):
