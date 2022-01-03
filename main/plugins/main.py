8c6e4cdc26465ecaf28373d03373236d13c76d07
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-03 15:38:32 +0530

    Update and rename start.py to main.py

---
+++ main/plugins/main.py
@@ -0,0 +1,21 @@
+#tg:ChauhanMahesh/DroneBots
+#github.com/vasusen-code
+
+from .. import Drone, ACCESS_CHANNEL, AUTH_USERS
+from telethon import events, Button
+from main.plugins.drive import drive
+from telethon.tl.types import MessageMediaWebPage
+
+@Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+async def u(event):
+    if not isinstance(event.media, MessageMediaWebPage):
+        return
+    if 'drive.google.com' in event.media.webpage.url: 
+        await event.reply('.', buttons=[[Button.inline("Upload.", data="drive")]])
+        
+@Drone.on(events.callbackquery.CallbackQuery(data="drive"))
+async def d(event):
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await drive(event, msg) 
+    
