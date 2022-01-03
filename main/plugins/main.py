404f14f963ea6725ad395207252628969d66d649
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-03 16:35:38 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -10,7 +10,7 @@ def get_link(string):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex,string)   
     try:
-        link = [x[0] for x in url]
+        link = [x[0] for x in url][0]
         if link:
             return link
         else:
