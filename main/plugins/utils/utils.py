92cae92a0b08a512932a04c1ad5cd7878cf65ddb
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-04 09:39:10 +0530

    Update utils.py

--- main/plugins/utils/utils.py
+++ main/plugins/utils/utils.py
@@ -44,10 +44,10 @@ async def upload_file(file, event, edit):
                 if result is False:
                     uploader = await fast_upload(file, file, time.time(), event.client, edit, f'**UPLOADING FILE:**')
                     await Drone.send_file(event.chat_id, uploader, caption=text, force_document=True)
-            else:
-                uploader = await fast_upload(file, file, time.time(), event.client, edit, f'**UPLOADING FILE:**')
-                await Drone.send_file(event.chat_id, uploader, caption=text, force_document=True)
-            os.remove(file)
+        else:
+            uploader = await fast_upload(file, file, time.time(), event.client, edit, f'**UPLOADING FILE:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, force_document=True)
+    os.remove(file)
     except Exception as e:
         return await edit.edit(f"An error `[{e}]` occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     await edit.delete()
