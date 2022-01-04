ed64a69a4b4eaee34c959c44a2d460a60c49cb2f
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-04 14:59:14 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -9,13 +9,13 @@ CHANNEL_LINK = "https://t.me/DroneBots"
 
 SUPPORT_LINK = "https://t.me/TeamDrone"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.3"   
+info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.1"   
 
-help_text = "Send me any link to Upload it's file to telegram.\n\nI can Upload from:\nDrive\nMega.nz\nWebPage Urls\nYT_dlp supported sites"
+help_text = "Send me any link to Upload it's file to telegram.\n\nI can Upload from:\nDrive\nMega.nz(soon)\nWebPage Urls\nYT_dlp supported sites"
 
 DEV = "https://t.me/MaheshChauhan"
 
-spam_notice = "This bot is hosted on heroku, and hence can run just one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+spam_notice = "This bot is hosted on heroku, and hence Spamming the bot will lead you to a ban."
 
 down_sticker = './LOCAL/downloading.tgs'
 
