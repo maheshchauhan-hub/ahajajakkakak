287d4ac801d955347777d523407a0328dd849c0e
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-04 08:09:11 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -4,8 +4,13 @@
 from .. import Drone
 from telethon import events, Button
 from main.plugins.drive import drive
+from main.plugins.youtubedl import ytdl
+from main.plugins.requests import weburl
 from main.plugins.utils.utils import get_link
 from LOCAL.localisation import link_animated
+
+async def upload_button(event, data):
+    await event.client.send_message(event.chat_id, file=link_animated, reply_to=event.id, buttons=[[Button.inline("Upload.", data=data)]])
 
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def u(event):
@@ -13,7 +18,9 @@ async def u(event):
     if link is False:
         return
     if 'drive.google.com' in link: 
-        await event.client.send_message(event.chat_id, file=link_animated, reply_to=event.id, buttons=[[Button.inline("Upload.", data="drive")]])
+        await upload_button(event, 'drive') 
+    else:
+        await upload_button(event, 'upload') 
         
 @Drone.on(events.callbackquery.CallbackQuery(data="drive"))
 async def d(event):
@@ -22,3 +29,7 @@ async def d(event):
     await event.delete()
     await drive(event, msg) 
     
+@Drone.on(events.callbackquery.CallbackQuery(data="upload"))
+async def d(event):
+    try:
+        
