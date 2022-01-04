e01e29b1cc1538eb483a7a1203a154b3195ce6c9
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-04 08:17:38 +0530

    Update requests.py

--- main/plugins/requests.py
+++ main/plugins/requests.py
@@ -1,6 +1,17 @@
 import requests
 import re
 
+#Does the url contain a downloadable resource
+def is_downloadable(url):
+    h = requests.head(url, allow_redirects=True)
+    header = h.headers
+    content_type = header.get('content-type')
+    if 'text' in content_type.lower():
+        return False
+    if 'html' in content_type.lower():
+        return False
+    return True
+
 #Get filename from content-disposition
 def get_filename_from_cd(cd):
     if not cd:
@@ -11,6 +22,13 @@ def get_filename_from_cd(cd):
     return fname[0]
   
 def weburl(url):
+    x = is_downloadable(url)
+    if x is False:
+        return None
+    elif x is True:
+        pass
+    else:
+        return None
     r = requests.get(url, allow_redirects=True)
     filename = get_filename_from_cd(r.headers.get('content-disposition'))
     open(filename, 'wb').write(r.content)
