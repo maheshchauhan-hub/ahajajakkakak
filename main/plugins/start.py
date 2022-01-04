f9553ffe1a23165f542e871619cb470e7145f4a0
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-04 11:28:06 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -6,8 +6,5 @@ from main.plugins.actions import set_thumbnail, rem_thumbnail, heroku_restart
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply(f'{st}', 
-                      buttons=[
-                              [Button.inline("Menu.", data="menu")]
-                              ])
-    
+    await event.reply(st)
+                    
