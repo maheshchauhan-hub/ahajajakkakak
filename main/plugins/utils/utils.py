dab60f36d1a9845cf723fe65783c3d5369e7d78e
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-04 15:04:40 +0530

    Update utils.py

--- main/plugins/utils/utils.py
+++ main/plugins/utils/utils.py
@@ -4,7 +4,9 @@
 import re
 import os
 import time
-from ... import BOT_UN
+import heroku3
+from ... import BOT_UN, MONGODB_URI
+from main.Database.database import Database
 from telethon import events
 from ethon.telefunc import fast_upload
 from telethon.tl.types import DocumentAttributeVideo
@@ -95,3 +97,46 @@ def get_link(string):
             return False
     except Exception:
         return False
+      
+#Thumbnail--------------------------------------------------------------------------------------------------------------
+
+async def set_thumbnail(event, img):
+    edit = await event.client.send_message(event.chat_id, 'Trying to process.')
+    try:
+        path = await event.client.download_media(img)
+        meta = upload_file(path)
+        link = f'https://telegra.ph{meta[0]}'
+    except Exception as e:
+        print(e)
+        return await edit.edit("Failed to Upload on Tgraph.")
+    await db.update_thumb_link(event.sender_id, link)
+    await edit.edit("Done!")
+    
+async def rem_thumbnail(event):
+    edit = await event.client.send_message(event.chat_id, 'Trying.')
+    T = await db.get_thumb(event.sender_id)
+    if T is None:
+        return await edit.edit('No thumbnail saved!')
+    await db.rem_thumb_link(event.sender_id)
+    await edit.edit('Removed!')
+    
+#Heroku--------------------------------------------------------------------------------------------------------------
+   
+async def heroku_restart():
+    HEROKU_API = config("HEROKU_API", default=None)
+    HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
+    x = None
+    if not HEROKU_API and HEROKU_APP_NAME:
+        x = None
+    else:
+        try:
+            acc = heroku3.from_key(HEROKU_API)
+            bot = acc.apps()[HEROKU_APP_NAME]
+            bot.restart()
+            x = True
+        except Exception as e:
+            print(e)
+            x = False
+    return x
+    
+
