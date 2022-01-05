0373b42512921ed8e7615f35e4b2b66cb0d9bf9a
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-05 21:50:24 +0530

    Update and rename youtubedl.py to ytdlp.py

--- main/plugins/ytdlp.py
+++ main/plugins/ytdlp.py
@@ -36,24 +36,7 @@ def ytdl(url):
         video_ext = info_dict.get('ext', None) 
         return video_title + '.' + video_ext
 
-#For youtubedownload------------------------------------------------------------------------------------------------
 
-from youtube_dl import YoutubeDL as YTDL
-
-async def youtube(url):
-    options = {
-        "geo-bypass": True,
-        "nocheckcertificate": True,
-        "outtmpl": "%(title)s.%(ext)s",
-        "format": "best",
-        "quiet": True }
-    YTDL(options).download([url])
-    info_dict = YTDL({}).extract_info(url=url, download=False)
-    video_title = info_dict.get('title', None) 
-    video_ext = info_dict.get('ext', None) 
-    return video_title + '.' + video_ext
-    
-    
     
     
         
