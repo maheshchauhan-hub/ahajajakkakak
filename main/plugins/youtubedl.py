1d8aa9ba83788e4fff96b73b9aaf3235e9e513ab
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-05 19:33:26 +0530

    Update youtubedl.py

--- main/plugins/youtubedl.py
+++ main/plugins/youtubedl.py
@@ -1,6 +1,7 @@
-from yt_dlp import YoutubeDL
+import youtube_dl
 import yt_dlp
-
+from yt_dlp import YoutubeDL
+from youtube_dl import YoutubeDL
 #logging-------------------------------------------------------------------------------------------
 
 class YTLogger:
@@ -29,6 +30,14 @@ ydlp_opts={'logger': YTLogger(),
 
 def ytdl(url):
     with yt_dlp.YoutubeDL(ydlp_opts) as ydl:
+        ydl.download([url])
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict.get('title', None) 
+        video_ext = info_dict.get('ext', None) 
+        return video_title + '.' + video_ext
+    
+def youtube(url):
+    with youtube_dl.YoutubeDL(ydlp_opts) as ydl:
         ydl.download([url])
         info_dict = ydl.extract_info(url, download=False)
         video_title = info_dict.get('title', None) 
