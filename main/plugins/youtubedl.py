a622440383142399897dd01a45767590bda7d6cd
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-05 21:18:49 +0530

    Update youtubedl.py

--- main/plugins/youtubedl.py
+++ main/plugins/youtubedl.py
@@ -38,7 +38,7 @@ def ytdl(url):
 
 #For youtubedownload------------------------------------------------------------------------------------------------
 
-import youtube_dl 
+from youtube_dl import YoutubeDL as YTDL
 
 async def youtube(url):
     options = {
@@ -47,8 +47,8 @@ async def youtube(url):
         "outtmpl": "%(title)s.%(ext)s",
         "format": "best",
         "quiet": True }
-    with youtube_dl.YoutubeDL(ydlp_opts) as ydl:
-        ydl.download([url])
+    YTDL(opts).download([url])
+    with yt_dlp.YoutubeDL({}) as ydl:
         info_dict = ydl.extract_info(url, download=False)
         video_title = info_dict.get('title', None) 
         video_ext = info_dict.get('ext', None) 
