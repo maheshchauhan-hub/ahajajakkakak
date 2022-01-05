38b057a8c722cd285196d752ae1d53e42c536d7e
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-05 19:53:49 +0530

    Update youtubedl.py

--- main/plugins/youtubedl.py
+++ main/plugins/youtubedl.py
@@ -35,7 +35,8 @@ def ytdl(url):
         video_title = info_dict.get('title', None) 
         video_ext = info_dict.get('ext', None) 
         return video_title + '.' + video_ext
-    
+
+#fucking slow   
 def youtube(url):
     with youtube_dl.YoutubeDL(ydlp_opts) as ydl:
         ydl.download([url])
