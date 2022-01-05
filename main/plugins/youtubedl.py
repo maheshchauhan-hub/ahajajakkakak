16da7e06b0b1ddbca36186648f6b45616fd1db68
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-06 00:01:40 +0530

    Update youtubedl.py

--- main/plugins/youtubedl.py
+++ main/plugins/youtubedl.py
@@ -1,7 +1,9 @@
+#github.com/teamultroid
+
 from pyUltroid.functions.ytdl import download_yt
 
 #For youtube videos download
-def youtube(url, event):
+async def youtube(url, event):
     options = {
         "nocheckcertificate": True,
         "geo-bypass": True,
@@ -9,5 +11,5 @@ def youtube(url, event):
         "format": "best",
         "quiet": True }
     options["postprocessors"] = [{"key": "FFmpegMetadata"}]
-    await download_yt(edit, url, options) 
+    await download_yt(event, url, options) 
     
