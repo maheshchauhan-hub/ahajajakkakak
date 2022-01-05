f4c12e9bb7926d4f44ace2e595957766295cda38
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-06 00:36:22 +0530

    Update youtubedl.py

--- main/plugins/youtubedl.py
+++ main/plugins/youtubedl.py
@@ -1,18 +1,19 @@
-from ethon.pyfunc import bash
+import os
+from youtube_dl import YoutubeDL
 
 #For youtube videos download
-async def youtube(url, event):    
-    try:
-        bash("pip install pyUltroid")
-        from pyUltroid.functions.ytdl import download_yt
-    except Exception:
-        pass
+def youtube(url, event):    
     options = {
         "nocheckcertificate": True,
         "geo-bypass": True,
-        "outtmpl": "%(id)s.mp4",
+        "outtmpl": "%(title)s.%(ext)s",
         "format": "best",
         "quiet": True }
     options["postprocessors"] = [{"key": "FFmpegMetadata"}]
-    await download_yt(event, url, options) 
+    YoutubeDL(options).download([url])
+    info = YoutubeDL({}).extract_info(url=url, download=False)
+    title = info["title"]
+    ext = "." + ytd["outtmpl"].split(".")[-1]
+    file = title + ext
+    return file
     
