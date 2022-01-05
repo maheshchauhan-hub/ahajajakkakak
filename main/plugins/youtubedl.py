385d3108512539270c065ed3f978f223241f4b36
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-05 21:03:04 +0530

    Update youtubedl.py

--- main/plugins/youtubedl.py
+++ main/plugins/youtubedl.py
@@ -1,9 +1,12 @@
-import youtube_dl
+#tg:MaheshChauhan/DroneBots
+#Github.com/Vasusen-code
+
 import yt_dlp
 from yt_dlp import YoutubeDL
-from youtube_dl import YoutubeDL
-#logging-------------------------------------------------------------------------------------------
 
+#for ytdlp supported sites -------------------------------------------------------------------------------------------
+
+#logging
 class YTLogger:
     def debug(self, msg):
         if msg.startswith('[debug] '):
@@ -20,13 +23,10 @@ class YTLogger:
     def error(self, msg):
         print(msg)
         
-#----------------------------------------------------------------------------------------------------------
-
 ydlp_opts={'logger': YTLogger(),
           'outtmpl': '%(title)s.%(ext)s',
           'no_warnings': True, 
           'quiet': True }
-
 
 def ytdl(url):
     with yt_dlp.YoutubeDL(ydlp_opts) as ydl:
@@ -36,11 +36,28 @@ def ytdl(url):
         video_ext = info_dict.get('ext', None) 
         return video_title + '.' + video_ext
 
-#fucking slow   
-def youtube(url):
+#For youtubedownload------------------------------------------------------------------------------------------------
+
+from youtube_dl 
+
+async def youtube(url):
+    options = {
+        "geo-bypass": True,
+        "nocheckcertificate": True,
+        "outtmpl": "%(title)s.%(ext)s",
+        "format": "best",
+        "quiet": True }
     with youtube_dl.YoutubeDL(ydlp_opts) as ydl:
         ydl.download([url])
         info_dict = ydl.extract_info(url, download=False)
         video_title = info_dict.get('title', None) 
         video_ext = info_dict.get('ext', None) 
         return video_title + '.' + video_ext
+    
+    
+    
+    
+        
+    
+    
+
