6a24876dcdf7e5747a38f9b45edd5023057d8731
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-05 15:10:01 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,16 +9,16 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
-AUTH_USERS = config("AUTH_USERS", default=None, cast=int)
-LOG_CHANNEL = config("LOG_CHANNEL", default=None)
-LOG_ID = config("LOG_ID", default=None)
-FORCESUB = config("FORCESUB", default=None)
-FORCESUB_UN = config("FORCESUB_UN", default=None)
-ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None)
-MONGODB_URI = config("MONGODB_URI", default=None)
-
+API_ID = int(4796990)
+API_HASH = "32b6f41a4bf740efed2d4ce911f145c7"
+BOT_TOKEN = "5089347286:AAEmf4L1IWrDMsOVpWcr7XI8ZE6F4LB_05s"
+BOT_UN = "@TheUploaderProBot"
+AUTH_USERS = int(1295012425)
+LOG_CHANNEL = "VcBotLog"
+LOG_ID = int(-1001180718837)
+FORCESUB = int(-1001485129059)
+FORCESUB_UN = "DroneBots"
+ACCESS_CHANNEL = int(-1001683603362)
+MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
+    
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
