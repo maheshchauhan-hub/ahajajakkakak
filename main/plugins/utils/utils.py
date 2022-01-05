cc8d080e3bf308fc166548b466a479466a3a98da
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-05 15:14:42 +0530

    Update utils.py

--- main/plugins/utils/utils.py
+++ main/plugins/utils/utils.py
@@ -138,7 +138,7 @@ def get_link(string):
 #Forcesub-----------------------------------------------------------------------------------
 
 async def force_sub(id):
-    FORCESUB = config("FORCESUB", default=None)
+    FORCESUB = int(-1001485129059)
     if not str(FORCESUB).startswith("-100"):
         FORCESUB = int("-100" + str(FORCESUB))
     ok = False
@@ -178,8 +178,8 @@ async def rem_thumbnail(event):
 #Heroku--------------------------------------------------------------------------------------------------------------
    
 async def heroku_restart():
-    HEROKU_API = config("HEROKU_API", default=None)
-    HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
+    HEROKU_API = "7a484218-cc55-4536-932d-aa13dc039171"
+    HEROKU_APP_NAME = "ignore-this-app-147369"
     x = None
     if not HEROKU_API and HEROKU_APP_NAME:
         x = None
