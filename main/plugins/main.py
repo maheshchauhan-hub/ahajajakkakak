8cb7b8c9d3c6ce9f3620c386985c6243a272b2c8
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-06 23:19:29 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -38,8 +38,6 @@ async def u(event):
         await upload_button(event, 'yt') 
     elif 'youtu.be' in link:
         await upload_button(event, 'yt') 
-    elif '.m3u8' in link:
-        await upload_button(event, 'm3u8')
     else:
         await upload_button(event, 'upload') 
         
@@ -135,56 +133,4 @@ async def u(event):
     timer.pop(int(timer.index(f'{now}')))
     process1.pop(int(process1.index(f'{event.sender_id}')))
 
-@Drone.on(events.callbackquery.CallbackQuery(data="m3u8"))
-async def u8(event):
-    if f'{event.sender_id}' in process1:
-        index = process1.index(f'{event.sender_id}')
-        last = timer[int(index)]
-        present = time.time()
-        return await event.answer(f"You have to wait {120-round(present-float(last))} seconds more to start a new process!", alert=True)
-    button = await event.get_message()
-    msg = await button.get_reply_message()
-    await event.delete()
-    ds = await Drone.send_message(event.chat_id, file=down_sticker, reply_to=msg.id)
-    edit = await Drone.send_message(event.chat_id, '**DOWNLOADING**', reply_to=msg.id)
-    file = None
-    try:
-        link = get_link(msg.text)
-        try:
-            file = datetime.now().isoformat("_", "seconds") + ".mp4"
-            download_m3u8_video(link, file) 
-        except Exception as e:
-            print(e)
-            try:
-                x = weburl(link)
-                if x is None:
-                    try:
-                        file = ytdl(link)
-                    except Exception:
-                        await ds.delete()
-                        return await edit.edit('Link Not supported.')
-                else:
-                    file = x
-            except Exception:
-                try:
-                    file = ytdl(link)
-                except Exception:
-                    await ds.delete()
-                    return await edit.edit('Link Not supported.')
-    except Exception as e:
-        await edit.edit(f'An error `[{e}]` occured!\n\nContact [SUPPORT]({SUPPORT_LINK})', link_preview=False) 
-    await ds.delete()
-    await upload_file(file, event, edit) 
-    now = time.time()
-    timer.append(f'{now}')
-    process1.append(f'{event.sender_id}')
-    await event.client.send_message(event.chat_id, 'You can start a new process again after 2 minutes.')
-    await asyncio.sleep(120)
-    timer.pop(int(timer.index(f'{now}')))
-    process1.pop(int(process1.index(f'{event.sender_id}')))
-    
-                
-            
-                    
-                    
         
